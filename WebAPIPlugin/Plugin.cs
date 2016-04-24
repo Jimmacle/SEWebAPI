5ef0956cdda8bea3d096e8ab19955b17594d4284
John Michael Gross <mindstorms11@cox.net>
2016-04-23 23:00:25 -0700

    Actually save the file this time

--- WebAPIPlugin/Plugin.cs
+++ WebAPIPlugin/Plugin.cs
@@ -114,27 +114,4 @@ namespace WebAPIPlugin
             }
         }
     }
-
-    public static class Extensions
-    {
-        public static void Respond(this HttpListenerContext context, object data, int statusCode)
-        {
-            try
-            {
-                var response = context.Response;
-                response.StatusCode = statusCode;
-
-                string serialized = JsonConvert.SerializeObject(data, Formatting.Indented);
-                byte[] bytes = Encoding.ASCII.GetBytes(serialized);
-                response.ContentLength64 = bytes.Length;
-
-                response.OutputStream.Write(bytes, 0, bytes.Length);
-                response.OutputStream.Close();
-            }
-            catch
-            {
-                MySandboxGame.Log.WriteLineAndConsole("SEWA: Error writing stream");
-            }
-        }
-    }
 }
